01c3e86707cdd24edddc65110f3e5cf6518ca634
Miguel Arrojo <miguel2002jnt@gmail.com>
2024-05-06 15:24:32 +0200

    headerv2 script imagenes nuevas y hoja estilos

--- js/script.js
+++ js/script.js
@@ -28,12 +28,41 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const togglers = document.querySelectorAll('.toggler');
-  togglers.forEach(toggler => {
-      toggler.addEventListener('click', function() {
-          const overlay = this.nextElementSibling;
-          overlay.style.display = overlay.style.display === 'flex' ? 'none' : 'flex';
-      });
-  });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   const preheaderToggler = document.querySelector('.preheader-toggler');
+//   const navbarToggler = document.querySelector('.navbar-toggler');
+//   const preheaderMenu = document.querySelector('.preheader-menu');
+//   const navbarMenu = document.querySelector('.menu');
+
+//   // Función para alternar el menú y el icono del toggler
+//   function toggleMenu(toggler, menu) {
+//       // Cerrar el otro menú si está abierto
+//       if (menu === preheaderMenu && navbarMenu.style.display === 'flex') {
+//           navbarMenu.style.display = 'none';
+//           navbarToggler.innerHTML = '☰';
+//       } else if (menu === navbarMenu && preheaderMenu.style.display === 'flex') {
+//           preheaderMenu.style.display = 'none';
+//           preheaderToggler.innerHTML = '☰';
+//       }
+
+//       // Alternar el menú actual
+//       if (menu.style.display === 'flex') {
+//           menu.style.display = 'none';
+//           toggler.innerHTML = '☰'; // Cambiar a menú burger
+//       } else {
+//           menu.style.display = 'flex';
+//           toggler.innerHTML = '✕'; // Cambiar a 'X' cuando esté abierto
+//       }
+//   }
+
+//   // Evento para el toggler del preheader
+//   preheaderToggler.addEventListener('click', function() {
+//       toggleMenu(this, preheaderMenu);
+//   });
+
+//   // Evento para el toggler de la barra de navegación principal
+//   navbarToggler.addEventListener('click', function() {
+//       toggleMenu(this, navbarMenu);
+//   });
+// });
+
